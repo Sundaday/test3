e32a5060f78915f4b1e46ff483d1b1185c403f7a
Sundaday <john.koch.pro@gmail.com>
2022-07-09 22:56:04 +0200

    end

--- test3/MainWindow.xaml.cs
+++ test3/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace test3
     public partial class MainWindow : Window
     {
         private readonly ViewModel viewModel;
+        private ObservableCollection<InfosTier> infos;
         public MainWindow()
         {
             InitializeComponent();
@@ -82,7 +83,23 @@ namespace test3
         private void SeeData(object sender, RoutedEventArgs e)
         {
             
+            
         }
-        
+        public List<InfosTier> GetTier()
+        {
+            InfosTier infosTier = new InfosTier();
+            try
+            {
+                ITiersService service = new TiersService("http://localhost:12345/Webservices100/BIJOU/");
+                List<Client> clients = service.ClientsList(1, 1);
+                var jsonString = JsonConvert.SerializeObject(clients);
+                //const string clientAsText = "[{\"TypeTiers\":0,\"NumeroTiers\":\"BAGUES\",\"NumeroTiersPayeur\":\"BAGUES\",\"IdCategorieComptable\":1,\"IdCategorieTarifaire\":1,\"IdModeleReglement\":4,\"Intitule\":\"Bague'senor\",\"Qualite\":\"\",\"Abrege\":\"Bague'senor\",\"Contact\":\"MlleLeeOndine\",\"CompteCollectif\":\"4110000\",\"Adresse\":\"RuetêtedOr\",\"Complement\":\"BP65993\",\"CodePostal\":\"57000\",\"Ville\":\"Metz\",\"Region\":\"Lorraine\",\"Pays\":\"France\",\"Telephone\":\"0665870000\",\"Fax\":\"0665870001\",\"Email\":\"bagues@bagues.fr\",\"SiteWeb\":\"bagues.com\",\"NumeroSiret\":\"02556852465474\",\"NumeroTva\":\"546789\",\"CodeEdi\":\"\",\"CodeNAF\":\"32.12Z\",\"Commentaire\":\"\",\"Langue\":0,\"EstEnSommeil\":false,\"IdDepot\":0,\"NumeroCentraleAchat\":null,\"CessionCreance\":false,\"EncoursAutorise\":50000.000000,\"IdRisque\":1,\"ModeControleEnCours\":0,\"AutomatiserEcritureMultiEcheances\":false,\"AutorisationLettrageAutomatique\":true,\"ExclureRappelsReleve\":false,\"Facebook\":\"\",\"LinkedIn\":\"\",\"InfosLibres\":[{\"Name\":\"Datecréationsociété\",\"Type\":2,\"Size\":0,\"EstCalculee\":false,\"Value\":\"/Date(549496800000+0200)/\"},{\"Name\":\"Capitalsocial\",\"Type\":3,\"Size\":0,\"EstCalculee\":false,\"Value\":0.000000},{\"Name\":\"ActionnairePal\",\"Type\":0,\"Size\":69,\"EstCalculee\":false,\"Value\":\"\"},{\"Name\":\"ScoreBanquedeFrance\",\"Type\":0,\"Size\":14,\"EstCalculee\":false,\"Value\":\"\"},{\"Name\":\"Totalpointsfidélité\",\"Type\":3,\"Size\":0,\"EstCalculee\":true,\"Value\":0.000000},{\"Name\":\"Pointsfidélitérestants\",\"Type\":3,\"Size\":0,\"EstCalculee\":true,\"Value\":0.000000},{\"Name\":\"Finvaliditécartefidélité\",\"Type\":1,\"Size\":0,\"EstCalculee\":false,\"Value\":\"/Date(1199055600000+0100)/\"},{\"Name\":\"Datenégociationrèglement\",\"Type\":2,\"Size\":0,\"EstCalculee\":false,\"Value\":\"/Date(1214776800000+0200)/\"},{\"Name\":\"TauxEscompteDefaut\",\"Type\":3,\"Size\":0,\"EstCalculee\":false,\"Value\":null},{\"Name\":\"Délaiescompteenjours\",\"Type\":3,\"Size\":0,\"EstCalculee\":false,\"Value\":null}],\"Createur\":\"ERP1\",\"DateModification\":\"/Date(1567675116000+0200)/\",\"ExclureTraitementMarketing\":false,\"GDPRActif\":false,\"IdDevise\":0,\"Statistique01\":\"Est\",\"Statistique02\":\"\",\"Statistique03\":\"\",\"Statistique04\":\"\",\"Statistique05\":\"\",\"Statistique06\":\"\",\"Statistique07\":\"\",\"Statistique08\":\"\",\"Statistique09\":\"\",\"Statistique10\":\"\",\"Facture\":1,\"Saut\":true,\"Analytique\":3,\"CodeAffaire\":\"953INDE\",\"DateCreation\":\"/Date(1104706800000+0100)/\",\"UtilisateurCreateur\":null}]";
+                List<InfosTier> infosTiers = JsonConvert.DeserializeObject<List<InfosTier>>(jsonString);
+                return myDeserializedClass;
+            }catch (Exception ex)
+            {
+
+            }
+        }
     }
 }
